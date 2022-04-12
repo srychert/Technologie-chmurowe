1c2d92c45c6cda2d558cde720570c2b9128b7e79
Sebastian Rychert <sebastian_rychert@wp.pl>
2022-04-12 11:28:54 +0200

    zadanie 2 redis

--- lab06/backend/server.js
+++ lab06/backend/server.js
@@ -3,11 +3,29 @@
 const express = require('express');
 
 const app = express();
+const test = require("./routes/test");
+
+app.use(express.json());
+app.use("/test", test);
+
+const client = require("./config/redisClient");
+
+client.on("error", (err) => {
+  console.error("Error connecting to Redis", err);
+});
+client.on("connect", () => {
+  console.log(`Connected to Redis.`);
+  const port = process.env.REDIS_PORT || 6379;
+  app.listen(port, () => {
+    console.log(`API server listening at http://localhost:${port}`);
+  });
+});
 
 const PORT = process.env.PGPORT;
-
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send({
+    msg: 'Hello World'
+  });
 });
 
 app.listen(PORT, () => {
