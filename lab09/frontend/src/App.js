3ce69b6d8140c8cdba078fa7be4305db937f878c
Sebastian Rychert <sebastian_rychert@wp.pl>
2022-05-17 22:14:24 +0200

    zad3 connect frontend to backend

--- lab09/frontend/src/App.js
+++ lab09/frontend/src/App.js
@@ -1,7 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [data, setData] = useState('')
+
+  useEffect(() => {
+    fetch('http://localhost:31100')
+      .then(response => {
+        response.text().then(text => {
+          console.log(text);
+          setData(text);
+        })
+      })
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
@@ -11,6 +24,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         Frontend - some changes here...
+        {data}
       </header>
     </div>
   );
